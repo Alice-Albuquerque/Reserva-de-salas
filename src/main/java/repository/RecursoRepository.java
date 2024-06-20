f5397122aaf564f9556b20e089391b7c41122fce
Ícaro Reis da Paixão <106492577+icaropaixao@users.noreply.github.com>
2024-06-19 21:41:02 -0300

    Delete src directory

--- src/main/java/repository/RecursoRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Recurso;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface RecursoRepository extends JpaRepository<Recurso, Long> {
-}
--- src/main/java/repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import model.Reserva;
-import java.util.List;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-    List<Reserva> findBySalaId(Long idSala);
-}
